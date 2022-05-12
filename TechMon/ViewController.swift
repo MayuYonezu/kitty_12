764e1b8cf0c71c0b20d731d88e3ba9dfe6c6e203
MayuYonezu <s2122054@stu.musashino-u.ac.jp>
2022-05-12 23:06:21 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Mayu Yonezu on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
